19743ca2ceefdf2c7ee3f0b42c3abcd628b02522
Egor <egorobidin@inbox.ru>
2023-09-22 16:41:37 +0300

    lab2 добавлены данные для lab1. Доделано weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -1,36 +1,73 @@
-import http.client
+import requests
+from datetime import datetime
 
-# Замените <значение ключа> и параметры запроса на конкретные значения
-api_key = "54d73608-c8dd-4c98-b18f-d9a5056525ab"
-lat = "55.75396"
-lon = "37.620393"
-
-# Создайте соединение с сервером
-conn = http.client.HTTPSConnection("api.weather.yandex.ru")
-
-# Формируйте URL запроса
-url = f"/v2/informers?lat={lat}&lon={lon}"
-
-# Создайте заголовок с ключом аутентификации
-headers = {
-    "X-Yandex-API-Key": api_key
+DIRECTION_TRANSFORM = {
+    'n': 'северное',
+    'nne': 'северо - северо - восточное',
+    'ne': 'северо - восточное',
+    'ene': 'восточно - северо - восточное',
+    'e': 'восточное',
+    'ese': 'восточно - юго - восточное',
+    'se': 'юго - восточное',
+    'sse': 'юго - юго - восточное',
+    's': 'южное',
+    'ssw': 'юго - юго - западное',
+    'sw': 'юго - западное',
+    'wsw': 'западно - юго - западное',
+    'w': 'западное',
+    'wnw': 'западно - северо - западное',
+    'nw': 'северо - западное',
+    'nnw': 'северо - северо - западное',
+    'c': 'штиль',
 }
 
-# Отправьте GET-запрос
-conn.request("GET", url, headers=headers)
 
-# Получите ответ
-response = conn.getresponse()
+def current_weather(lat, lon):
+    """
+    Описание функции, входных и выходных переменных
+    """
+    token = '54d73608-c8dd-4c98-b18f-d9a5056525ab'
+    url = f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}"
+    headers = {"X-Yandex-API-Key": f"{token}"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-# Проверьте статус ответа
-if response.status == 200:
-    data = response.read()
-    # Обработка данных о погоде (data содержит ответ в байтах)
-    print(data.decode('utf-8'))  # Преобразование ответа в строку
-else:
-    print(f"Ошибка при запросе: {response.status} {response.reason}")
+    result = {
+        'city': data['geo_object']['locality']['name'],
+        'time': datetime.fromtimestamp(data['fact']['uptime']).strftime("%H:%M"),
+        'temp': data['fact']['temp'],
+        'feels_like_temp': data['fact']['feels_like'],
+        'pressure': data['fact']['pressure_mm'],
+        'humidity': data['fact']['humidity'],
+        'wind_speed': data['fact']['wind_speed'],
+        'wind_gust': data['fact']['wind_gust'],
+        'wind_dir': DIRECTION_TRANSFORM.get(data['fact']['wind_dir']),
+    }
+    return result
 
-# Закройте соединение
-conn.close()
+
+# def current_weather(lat, lon):
+#     """
+#     Описание функции, входных и выходных переменных
+#     """
+#     token = 'b4588e45fcc14bdb89d63200232109'
+#     url = f"https://api.weatherapi.com/v1/current.json?key={token}&q={lat},{lon}"
+#     response = requests.get(url)
+#     data = response.json()
+#
+#     result = {
+#         'city': data['location']['name'],
+#         'time': data['current']['last_updated'].split()[-1],
+#         'temp': data['current']['temp_c'],
+#         'feels_like_temp': data['current']['feelslike_c'],
+#         'pressure': round(data['current']['pressure_mb'] * 0.75, 1),
+#         'humidity': data['current']['humidity'],
+#         'wind_speed': round(data['current']['wind_kph'] / 3.6, 1),
+#         'wind_gust': round(data['current']['gust_kph'] / 3.6, 1),
+#         'wind_dir': DIRECTION_TRANSFORM.get(data['current']['wind_dir'].lower()),
+#     }
+#     return result
 
 
+if __name__ == "__main__":
+    print(current_weather(59.93, 30.31))
