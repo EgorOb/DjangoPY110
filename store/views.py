0b96ee857c87f539d38268bebce101a0ed937685
Egor <egorobidin@inbox.ru>
2023-10-02 00:19:29 +0300

    lab3

--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseNotFound
 from .models import DATABASE
 
 
 def products_view(request):
     if request.method == "GET":
+        if id_product := request.GET.get("id"):
+            if data := DATABASE.get(id_product):
+                return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                             'indent': 4})
+            return HttpResponseNotFound("Данного продукта нет в базе данных")
+
         return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
                                                          'indent': 4})
 
